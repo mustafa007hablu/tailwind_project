e517a982f9dfb9dd5f7379b83b572923c84dcddf
mustafa007hablu <mustafa_glory007@yahoo.com>
2024-11-09 23:59:26 +0600

    full project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = { darkMode: 'class',
+  content: ['*'],
+  theme: {
+    container: {center: true},
+    extend: {},
+  },
+  plugins: [],
+}
+
